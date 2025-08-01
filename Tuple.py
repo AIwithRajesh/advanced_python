981b6102717a74d0c2d428d6260cb781ad874cf8
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-01 23:37:24 +0530

    update: tuple started

---
+++ Tuple.py
@@ -0,0 +1,10 @@
+# Tuple: ordered, immutable, allows duplicate elements
+# mytuple = ("MAx",) # this will recognize as string you have to add , after that
+# print(type(mytuple))
+
+mytuple = tuple(["max", 28, "Boston"]) #we can create tuple using tuple built in function
+print(mytuple)
+
+# IF you want access element
+item = mytuple[0]
+print(item)
