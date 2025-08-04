b7896957562d2da1bc8375049407bb1bed815e2e
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-04 23:53:46 +0530

    update: string slicing

--- String.py
+++ String.py
@@ -1,1 +1,10 @@
 # Stings: ordered, immutable , text representation
+
+my_string = "hello, World"
+
+# slicing
+# substring = my_string[1:7]
+# substring = my_string[:5] start from begging
+# substring = my_string[::2] takes every second char
+substring = my_string[::-1] #reverse the string
+print(substring)
