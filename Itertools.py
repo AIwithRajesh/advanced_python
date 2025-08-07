40ab45e6824b863d6104911830eabf718bed1281
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-08 01:00:33 +0530

    update: learned product, permutations in Itertools

---
+++ Itertools.py
@@ -0,0 +1,35 @@
+# itertools: product, permutations, accumulate, groupby, and infinite iterators
+
+from itertools import product
+# Product returs the all possible combinations (including repeated number) of givem items in iterables 
+a: list[int] = [1,2]
+b: list[int] = [3]
+
+prod = product(a,b, repeat=2) 
+print(list(prod))  #OUTPUT: [(1, 3), (1, 4), (2, 3), (2, 4)]
+
+# REAL WORLD USE CASE
+
+cloth_color: list[str] = ['Red', 'Blue']
+size: list[str] = ['M', "S"]
+
+for combo in product(cloth_color, size):
+    print(combo) # OUTPUT ('Red', 'M')
+                        # ('Red', 'S')
+                        # ('Blue', 'M')
+                        # ('Blue', 'S')
+
+# # BRUTE FORCE PROBLEM
+# for code in product('0123456789', repeat=4 ):
+#     print(''.join(code))
+
+from itertools import permutations
+# RETURNS ALL POSSIBLES ORDERINGS OF GIVEN INPUTS
+
+a: list[str] = [1,2,3]
+perm = permutations(a)
+
+# print(list(perm)) #OUTPUT: [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
+
+perm = permutations(a, 2) # Give can give as second parameter as length
+print(list(perm)) #OUTPUT: [(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)]
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,2 +1,1 @@
-m.maps.append({'y': 5, 'z': 6}) # this will on index level
-# print(cm) #OUTPUT: ChainMap({'a': 1, 'b': 2}, {'b': 3, 'c': 4}, {'y': 5, 'z': 6})
+ 
