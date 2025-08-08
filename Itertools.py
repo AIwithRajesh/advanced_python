b4a968aff0b12598e9bd2802949764646970188a
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-08 23:34:02 +0530

    update: combinations and combinations with replacement learned

--- Itertools.py
+++ Itertools.py
@@ -5,31 +5,53 @@ from itertools import product
 a: list[int] = [1,2]
 b: list[int] = [3]
 
-prod = product(a,b, repeat=2) 
-print(list(prod))  #OUTPUT: [(1, 3), (1, 4), (2, 3), (2, 4)]
+# prod = product(a,b, repeat=2) 
+# print(list(prod))  #OUTPUT: [(1, 3), (1, 4), (2, 3), (2, 4)]
 
-# REAL WORLD USE CASE
+# # REAL WORLD USE CASE
 
-cloth_color: list[str] = ['Red', 'Blue']
-size: list[str] = ['M', "S"]
+# cloth_color: list[str] = ['Red', 'Blue']
+# size: list[str] = ['M', "S"]
 
-for combo in product(cloth_color, size):
-    print(combo) # OUTPUT ('Red', 'M')
-                        # ('Red', 'S')
-                        # ('Blue', 'M')
-                        # ('Blue', 'S')
+# for combo in product(cloth_color, size):
+#     print(combo) # OUTPUT ('Red', 'M')
+#                         # ('Red', 'S')
+#                         # ('Blue', 'M')
+#                         # ('Blue', 'S')
 
-# # BRUTE FORCE PROBLEM
-# for code in product('0123456789', repeat=4 ):
-#     print(''.join(code))
+# # # BRUTE FORCE PROBLEM
+# # for code in product('0123456789', repeat=4 ):
+# #     print(''.join(code))
 
 from itertools import permutations
-# RETURNS ALL POSSIBLES ORDERINGS OF GIVEN INPUTS
+# # RETURNS ALL POSSIBLES ORDERINGS OF GIVEN INPUTS IN THIS ORDER MATTERS
 
-a: list[str] = [1,2,3]
-perm = permutations(a)
+# a: list[str] = [1,2,3]
+# perm = permutations(a)
 
-# print(list(perm)) #OUTPUT: [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
+# # print(list(perm)) #OUTPUT: [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
 
-perm = permutations(a, 2) # Give can give as second parameter as length
-print(list(perm)) #OUTPUT: [(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)]
+# perm = permutations(a, 2) # Give can give as second parameter as length
+# print(list(perm)) #OUTPUT: [(1, 2), (1, 3), (2, 1), (2, 3), (3, 1), (3, 2)]
+
+from itertools import combinations, combinations_with_replacement
+arr: list[int] = [1,2,3,4]
+
+com: combinations = combinations(arr, 2)
+# RETURNS COMBINATIONS OF GIVEN INPUTS WITH NOT REPETED COMBINATIONS AND ORDER DOESN'T MATTER
+
+# print(list(com)) #OUTPUT: [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
+
+ice_cream: list[str] = ['Vanilla', "Strawberry", "Chocolate"]
+
+com_wr = combinations_with_replacement(ice_cream, 2)
+# ALL THE COMBINATIONS WITH REPEAT VALUES
+print(list(com_wr))
+#OUTPUT: [
+#     ('Vanilla', 'Vanilla'),
+#     ('Vanilla', 'Strawberry'),
+#     ('Vanilla', 'Chocolate'),
+#     ('Strawberry', 'Strawberry'), 
+#     ('Strawberry', 'Chocolate'),
+#     ('Chocolate', 'Chocolate')
+# ]
