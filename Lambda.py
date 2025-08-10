140480bc24ea801f6aac858a8377040ae4d2f597
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-10 23:57:59 +0530

    lambda function completed

--- Lambda.py
+++ Lambda.py
@@ -19,5 +19,28 @@ def sort_by_y(x):
     return x[1]
 
 # points2D_sorted = sorted(points2D, key=lambda x: x[1])
-points2D_sorted = sorted(points2D, key=sort_by_y)
-print(points2D_sorted) #OUTPUT: [(5, -1), (15, 1), (1, 2), (10, 4)]
+# points2D_sorted = sorted(points2D, key=sort_by_y)
+# print(points2D_sorted) #OUTPUT: [(5, -1), (15, 1), (1, 2), (10, 4)]
+
+# map(func, seq)
+# a: list[int] = [1,2,3,4,5]
+# b: object = map(lambda x: x*2, a)
+
+# print(list(b)) #OUTPUT: [2, 4, 6, 8, 10]
+
+# c = [x*2 for x in a] #This is very easy and recommended 
+# print(c) #OUTPUT: [2, 4, 6, 8, 10]
+
+#filter(func, seq)
+a: list[int] = [1,2,3,4]
+
+# b = filter(lambda x: x%2==0, a)
+# print(list(b)) #OUTPUT: [2, 4, 6]
+# c = [x for x in a if x %2 == 0]
+# print(c) #OUTPUT: [2, 4, 6]
+
+# reduce(func, seq)
+from functools import reduce
+
+product_a = reduce(lambda x,y: x*y, a)
+print(product_a) #OUTPUT 24
