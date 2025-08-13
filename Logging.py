5b02ab6414c2e784f226d64b7e8b204235992468
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-13 23:54:38 +0530

    update: logging started

---
+++ Logging.py
@@ -0,0 +1,31 @@
+import logging
+
+# logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# logging.debug('This is debug message')
+# logging.info('This is an info message')
+# logging.warning('This is a warning message')
+# logging.error('This is an error message')
+# logging.critical('This is critical message')
+
+logger = logging.getLogger(__name__)
+
+# logger.info('Custom logger')
+
+# create handler
+stream_h = logging.StreamHandler()
+file_h = logging.FileHandler('file.log')
+
+# level and the format
+stream_h.setLevel(logging.WARNING)
+file_h.setLevel(logging.ERROR)
+
+formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+stream_h.setFormatter(formatter)
+file_h.setFormatter(formatter)
+
+logger.addHandler(stream_h)
+logger.addHandler(file_h)
+
+logging.warning('This is a warning')
+logging.error('This is a error')
