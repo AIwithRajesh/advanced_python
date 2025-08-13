75eaab0f9ea94fedd6d52623869f7f5637eef131
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-14 00:28:46 +0530

    update: logging.conf created

--- Logging.py
+++ Logging.py
@@ -8,24 +8,37 @@ import logging
 # logging.error('This is an error message')
 # logging.critical('This is critical message')
 
-logger = logging.getLogger(__name__)
+# logger = logging.getLogger(__name__)
 
-# logger.info('Custom logger')
+# # logger.info('Custom logger')
 
-# create handler
-stream_h = logging.StreamHandler()
-file_h = logging.FileHandler('file.log')
+# # create handler
+# stream_h = logging.StreamHandler()
+# file_h = logging.FileHandler('file.log')
 
-# level and the format
-stream_h.setLevel(logging.WARNING)
-file_h.setLevel(logging.ERROR)
+# # level and the format
+# stream_h.setLevel(logging.WARNING)
+# file_h.setLevel(logging.ERROR)
 
-formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
-stream_h.setFormatter(formatter)
-file_h.setFormatter(formatter)
+# formatter = logging.Formatter('%(name)s - %(levelname)s - %(message)s')
+# stream_h.setFormatter(formatter)
+# file_h.setFormatter(formatter)
 
-logger.addHandler(stream_h)
-logger.addHandler(file_h)
+# logger.addHandler(stream_h)
+# logger.addHandler(file_h)
 
-logger.warning('This is a warning')
-logger.error('This is a error')
+# logger.warning('This is a warning')
+# logger.error('This is a error')
+
+# import logging.config
+# logging.config.dictConfig('logging.conf')
+
+# logger = logging.getLogger('simpleExample')
+# logger.debug('This is a debug message')
+import traceback
+
+try:
+    a = [1,2,3]
+    val = a[4]
+except IndexError as e:
+    logging.error("This error is %s", traceback.format_exc())
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,12 +1,1 @@
-
-# try:
-#     a = 5/1
-#     b = a + '10'
-# except ZeroDivisionError as e:
-#     print('an error',e)
-# except TypeError as e:
-#     print('type error', e)
-# else:
-#     print('everything is fine')
-# finally:
-#     print('cleaning..')
+config
