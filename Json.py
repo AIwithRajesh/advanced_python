32f99954f0c8342692d27686397ea68be3390dbc
AIwithRajesh <choudharyrajesh729@gmail.com>
2025-08-17 00:04:49 +0530

    update: json convert to string and string to json, isinstance used to check obj

--- Json.py
+++ Json.py
@@ -22,7 +22,34 @@ person = {
 
 # person_json = json.dumps(person, indent=4, separators=(': ', "="))
 person_json = json.dumps(person, indent=4, sort_keys=True)
-print(person_json)
+# print(person_json)
 
-with open('person.json', 'w') as file:
-    json.dump(person, file)
+# with open('person.json', 'w') as file:
+#   json.dump(person, file, indent=4)
+
+# with open('person.json', 'r') as file:
+#     person = json.load(file)
+#     print(person)
+
+# convert_to_dict = json.loads(person_json)
+# print(convert_to_dict)
+
+class User():
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+
+user = User("Alice", 29)
+
+def encode_user(obj):
+    if isinstance(obj, User):
+        return {
+            "name": obj.name,
+            "age": obj.age,
+            obj.__class__.__name__: "User"
+        }
+    else:
+        raise TypeError('Object of type is not json serializable')
+
+print(json.dumps(user, default=encode_user))
