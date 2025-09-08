7401a57ff13698b68615287c688b8b944cba0178
AiWithRajesh <choudharyrajesh729@gmail.com>
2025-09-08 09:36:33 +0530

    update: Queue completed

--- multiprocessing/multi.py
+++ multiprocessing/multi.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process, Value, Array, Lock
 # from threading import Thread
+from multiprocessing import Queue
 import os
 import time
 
@@ -58,15 +59,21 @@ def add_100(numbers, lock):
         # with lock:
         #     number.value += 1
 
+def square(numbers, queue):
+    for i in numbers:
+        queue.put(i*i)
+
+def make_negative(numbers, queue):
+    for i in numbers:
+        queue.put(-1 * i)
 
 if __name__ == '__main__':
-    lock = Lock()
-    # shared_number = Value('i', 0)
-    shared_array = Array('d', [0.0, 100.0,200.0])
-    print('shared_array at beginning is',shared_array[:])
 
-    p1 = Process(target=add_100, args=(shared_array,lock))
-    p2 = Process(target=add_100, args=(shared_array,lock))
+    numbers = range(1,6)
+    q = Queue()
+
+    p1 = Process(target=square, args=(numbers, q))
+    p2 = Process(target=make_negative, args=(numbers, q))
 
     p1.start()
     p2.start()
@@ -74,6 +81,23 @@ if __name__ == '__main__':
     p1.join()
     p2.join()
 
-    print('number at end is', shared_array[:])
+    while not q.empty():
+        print(q.get())
+
+    # lock = Lock()
+    # # shared_number = Value('i', 0)
+    # shared_array = Array('d', [0.0, 100.0,200.0])
+    # print('shared_array at beginning is',shared_array[:])
+
+    # p1 = Process(target=add_100, args=(shared_array,lock))
+    # p2 = Process(target=add_100, args=(shared_array,lock))
+
+    # p1.start()
+    # p2.start()
+
+    # p1.join()
+    # p2.join()
+
+    # print('number at end is', shared_array[:])
 
 
